037b1a4c26fee19381c235a48786a9adef495c6a
eske4 <eske.larsen@gmail.com>
2024-02-19 03:08:03 +0000

    added focus mode

--- lua/KlintoE/plugins/twillight.lua
+++ lua/KlintoE/plugins/twillight.lua
@@ -1,10 +1,13 @@
-return{
-    {
-        "folke/twilight.nvim",
-        opts = {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-    }
+return {
+	{
+		"folke/twilight.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>mf", ":Twilight<cr>")
+		end,
+	},
 }

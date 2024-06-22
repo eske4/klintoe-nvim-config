d1af64200d3e2134f43c1d45809a4f635bc64637
eske4 <eske.larsen@gmail.com>
2024-06-22 05:18:04 +0200

    added the plugin that previous commit didnt get

---
+++ lua/KlintoE/plugins/Util/code-snippet.lua
@@ -0,0 +1,15 @@
+return {
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+	main = "nvim-silicon",
+	config = function()
+		require("silicon").setup({
+			font = "FiraCode",
+			output = (os.getenv("HOME") or os.getenv("USERPROFILE"))
+				.. "/codesnips/"
+				.. os.date("!%Y-%m-%dT%H-%M-%SZ")
+				.. "_code.png",
+		})
+	end,
+}

0bcc16786802057608765f9b5bc9535cb7779f54
eske4 <eske.larsen@gmail.com>
2024-07-18 02:42:20 +0200

    Added script to install must have packages

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Ensure yay is installed
+if ! command -v yay &> /dev/null; then
+    echo "yay is not installed. Please install yay and rerun the script."
+    exit 1
+fi
+
+# Create directory for code snippets if it doesn't exist
+mkdir -p ~/codesnips
+
+# Install Lua, LuaCheck, Rust Analyzer, Luarocks, and Mercurial
+yay -S --noconfirm lua luacheck rust-analyzer luarocks mercurial
+
+# Install Lua packages
+luarocks install argparse
+luarocks install luafilesystem
+luarocks install jsregexp
+
+# Install additional packages
+yay -S --noconfirm ripgrep gzip unzip curl tar shellcheck tree-sitter-cli gcc clang dotnet-sdk flutter nodejs cmake r php ruby python-pynvim silicon tmux
+
+# Install tmux plugin manager
+git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+echo "All packages have been successfully installed."

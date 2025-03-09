73465d129f91ca37eca85e10d7d6002db843b182
eske4 <36681234+eske4@users.noreply.github.com>
2025-03-09 02:58:02 +0100

    Update remaps.lua

--- lua/KlintoE/remaps.lua
+++ lua/KlintoE/remaps.lua
@@ -5,9 +5,5 @@ vim.g.mapleader = " "
 -- Settings to improve Nvim but not shortcuts to remember--
 -----------------------------------------------------------
 
--- Enables movement of highlighted text and movement inside code blocks etc
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move highlighted items down in visual mode
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move highlighted items up in visual mode
-
 -- Fold shortcut to open or fold functions etc
 vim.keymap.set("n", "<C-f>", "za") -- Fold or unfold

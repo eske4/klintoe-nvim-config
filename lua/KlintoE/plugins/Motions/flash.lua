d1a9784655991c84ba93a8f6beca8432bdec0f8f
eske4 <36681234+eske4@users.noreply.github.com>
2025-03-10 01:56:44 +0100

    Update flash.lua

--- lua/KlintoE/plugins/Motions/flash.lua
+++ lua/KlintoE/plugins/Motions/flash.lua
@@ -1,7 +1,13 @@
 return {
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {},
-	},
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {
+            modes = {
+                search = {
+                    enabled = true, -- Enable Flash Search by default
+                },
+            },
+        },
+    },
 }
